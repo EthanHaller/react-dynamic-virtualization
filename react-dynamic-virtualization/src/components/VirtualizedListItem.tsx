505594b9419573f12dcd9a6cd907a978422586a3
Ethan Haller <ethanhaller02@gmail.com>
2026-09-26 15:56:38 +0200

    refactor: extract usePositions hook and add key to VirtualizedListItem (#25)

---
+++ react-dynamic-virtualization/src/components/VirtualizedListItem.tsx
@@ -0,0 +1,33 @@
+export type RenderItemOptions = {
+  ref: (element: HTMLDivElement | null) => void
+  style: React.CSSProperties
+}
+
+type VirtualizedListItemProps<T> = {
+  item: T
+  itemId: string
+  position: number
+  getItemRef: (itemId: string) => (element: HTMLDivElement | null) => void
+  renderItem: (item: T, options: RenderItemOptions) => React.ReactElement
+}
+
+function VirtualizedListItem<T>({
+  item,
+  itemId,
+  position,
+  getItemRef,
+  renderItem,
+}: VirtualizedListItemProps<T>) {
+  return renderItem(item, {
+    ref: getItemRef(itemId),
+    style: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      transform: `translateY(${position}px)`,
+    },
+  })
+}
+
+export default VirtualizedListItem
